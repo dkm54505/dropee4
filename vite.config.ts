82a029c86c841d7d40b9a482650bfafa05d0078c
Deepak Meena <ghunawatdk1@gmail.com>
2025-08-17 13:43:38 +0530

    Your commit message

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: 'client',            
+  root: 'client',
   build: {
-    outDir: '../dist',       
+    outDir: '../dist',
   },
 })
+
